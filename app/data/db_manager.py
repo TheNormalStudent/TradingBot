e881b5346f86c4e3dd5d515b4372943696537f16
ZuchonoK <zhukoff.slava@gmail.com>
2021-12-24 22:57:07 +0200

    FIrst OCR homework (creating classes and getting their values)

---
+++ app/data/db_manager.py
@@ -0,0 +1,9 @@
+class db_manager_cl:
+
+    def add_to_db(self):
+        line = "added to db"
+        return line
+    def pull_from_db(self):
+        line = "get from db"
+        return line
+
--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-#checking repository
+from data.db_manager import db_manager_cl
+
+db_manager = db_manager_cl()
+print(db_manager.add_to_db())
+print(db_manager.pull_from_db())
