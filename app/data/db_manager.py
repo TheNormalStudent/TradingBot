80d9b7d4d546a6c186d05bc2257aba85cd7c4542
ZuchonoK <zhukoff.slava@gmail.com>
2021-12-24 23:26:17 +0200

    adding commentaries to ready code

--- app/data/db_manager.py
+++ app/data/db_manager.py
@@ -1,8 +1,10 @@
-class db_manager_cl:
-
+#создаём класс db_manager_cl
+class db_manager_cl: 
+    #создаём метод add_to_db(возвращает строку)
     def add_to_db(self):
         line = "added to db"
         return line
+    #создаём метод pull_from_db(возвращает строку)
     def pull_from_db(self):
         line = "get from db"
         return line
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from data.db_manager import db_manager_cl
+from data.db_manager import db_manager_cl  #импортируем класс
 
-db_manager = db_manager_cl()
-print(db_manager.add_to_db())
-print(db_manager.pull_from_db())
+db_manager = db_manager_cl()               #создаём объект класса db_manager_cl
+print(db_manager.add_to_db())              #вызывем метод класса 
+print(db_manager.pull_from_db())           #вызывем метод класса 
