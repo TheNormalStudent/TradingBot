f936158b34208a84c0aa87f645cefe795e32d529
ZuchonoK <zhukoff.slava@gmail.com>
2021-12-29 00:57:12 +0200

    Working with DBs. Adding columns ID, ticker, price (no date)

--- app/data/db_manager.py
+++ app/data/db_manager.py
@@ -1,11 +1,31 @@
+import sqlite3
+
+
+
 #создаём класс db_manager_cl
 class db_manager_cl: 
     #создаём метод add_to_db(возвращает строку)
     def add_to_db(self):
-        line = "added to db"
-        return line
+        con = sqlite3.connect('app/data/data.db')
+        cursor = con.cursor()
+        sql_query ='''
+        INSERT INTO crypto_info (ticker, price)
+        VALUES ('BTCUSDT', 68000)
+        '''
+        cursor.execute(sql_query)
+        con.commit()
+        con.close
+
     #создаём метод pull_from_db(возвращает строку)
     def pull_from_db(self):
-        line = "get from db"
-        return line
+        con = sqlite3.connect('app/data/data.db')
+        cursor = con.cursor() 
+        sql_query ='''
+                SELECT * FROM [crypto_info] 
+                '''
+        cursor.execute(sql_query)
+        results = cursor.fetchall()
+        con.commit()
+        con.close()
+        return results
 
