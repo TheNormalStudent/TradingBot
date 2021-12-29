1e2fb8493cce3a8fb0f578fb3e98b0cb725c86cb
ZuchonoK <zhukoff.slava@gmail.com>
2021-12-29 22:08:49 +0200

    1)Adding date to SQL Insert

--- app/data/create_db.py
+++ app/data/create_db.py
@@ -1,21 +1,11 @@
 import sqlite3
-from datetime import datetime, date, time
-date = datetime.now()
-
 con = sqlite3.connect('app/data/data.db')
 cursor = con.cursor() 
+
 sql_query = '''
-        CREATE TABLE crypto_info(id INTEGER PRIMARY KEY, ticker TEXT, price INTEGER);
-        '''
-sql_query_2 ='''
-        INSERT INTO crypto_info (ticker, price)
-        VALUES ('BTCUSDT', 68000, "date")
-        '''
-sql_query_3 ='''
-        SELECT * FROM [crypto_info] 
+        CREATE TABLE crypto_info(id INTEGER PRIMARY KEY, ticker TEXT, price INTEGER,  date TEXT);
         '''
 cursor.execute(sql_query)
-results = cursor.fetchall()
-print(results)
 con.commit()
 con.close()
+        
--- app/data/db_manager.py
+++ app/data/db_manager.py
@@ -1,20 +1,24 @@
 import sqlite3
-
+from datetime import datetime, date, time
 
 
 #создаём класс db_manager_cl
 class db_manager_cl: 
     #создаём метод add_to_db(возвращает строку)
-    def add_to_db(self):
+    def add_to_db(self):       
         con = sqlite3.connect('app/data/data.db')
-        cursor = con.cursor()
-        sql_query ='''
-        INSERT INTO crypto_info (ticker, price)
-        VALUES ('BTCUSDT', 68000)
-        '''
-        cursor.execute(sql_query)
+        cursor = con.cursor() 
+        time = datetime.now()
+        data_to_db = []
+        data = {"ticker":"BTCUSDT", "price": 70000, "date": time}
+        for value in data.values():
+            data_to_db.append(value)
+        cursor.execute('''INSERT INTO crypto_info (ticker, price, date) VALUES (?,?,?)''', data_to_db)
+        results = cursor.fetchall()
         con.commit()
-        con.close
+        con.close()
+        return results
+
 
     #создаём метод pull_from_db(возвращает строку)
     def pull_from_db(self):
